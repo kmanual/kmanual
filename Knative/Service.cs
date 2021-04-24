051d37e90e4ec9f6c2e3f4e64537bbcbb6037f4b
Mahdi <mahdi3466@yahoo.com>
2021-04-25 00:12:51 +0430

    Knative.

---
+++ Knative/Service.cs
@@ -0,0 +1,119 @@
+﻿using k8s;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using k8s.Models;
+
+namespace Knative
+{
+    [KubernetesEntity(Group = "serving.knative.dev", Kind = "Service", ApiVersion = "v1", PluralName = "services")]
+    public partial class Service : IKubernetesObject<V1ObjectMeta>, ISpec<ServiceSpec>, IValidate
+    {
+        /// <summary>
+        /// Initializes a new instance of the Service class.
+        /// </summary>
+        public Service()
+        {
+            CustomInit();
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the Service class.
+        /// </summary>
+        /// <param name="apiVersion">APIVersion defines the versioned schema of
+        /// this representation of an object. Servers should convert recognized
+        /// schemas to the latest internal value, and may reject unrecognized
+        /// values. More info:
+        /// https://git.k8s.io/community/contributors/devel/sig-architecture/api-conventions.md#resources</param>
+        /// <param name="kind">Kind is a string value representing the REST
+        /// resource this object represents. Servers may infer this from the
+        /// endpoint the client submits requests to. Cannot be updated. In
+        /// CamelCase. More info:
+        /// https://git.k8s.io/community/contributors/devel/sig-architecture/api-conventions.md#types-kinds</param>
+        /// <param name="metadata">Standard object's metadata. More info:
+        /// https://git.k8s.io/community/contributors/devel/sig-architecture/api-conventions.md#metadata</param>
+        /// <param name="spec">Specification of the desired behavior of the
+        /// service. More info:
+        /// https://git.k8s.io/community/contributors/devel/sig-architecture/api-conventions.md#spec-and-status</param>
+        /// <param name="status">Most recently observed status of the service. This
+        /// data may not be up to date. Populated by the system. Read-only.
+        /// More info:
+        /// https://git.k8s.io/community/contributors/devel/sig-architecture/api-conventions.md#spec-and-status</param>
+        public Service(string apiVersion = default, string kind = default, V1ObjectMeta metadata = default, ServiceSpec spec = default, ServiceStatus status = default)
+        {
+            ApiVersion = apiVersion;
+            Kind = kind;
+            Metadata = metadata;
+            Spec = spec;
+            Status = status;
+            CustomInit();
+        }
+
+        /// <summary>
+        /// An initialization method that performs custom operations like setting defaults
+        /// </summary>
+        partial void CustomInit();
+
+        /// <summary>
+        /// Gets or sets aPIVersion defines the versioned schema of this
+        /// representation of an object. Servers should convert recognized
+        /// schemas to the latest internal value, and may reject unrecognized
+        /// values. More info:
+        /// https://git.k8s.io/community/contributors/devel/sig-architecture/api-conventions.md#resources
+        /// </summary>
+        [JsonProperty(PropertyName = "apiVersion")]
+        public string ApiVersion { get; set; }
+
+        /// <summary>
+        /// Gets or sets kind is a string value representing the REST resource
+        /// this object represents. Servers may infer this from the endpoint
+        /// the client submits requests to. Cannot be updated. In CamelCase.
+        /// More info:
+        /// https://git.k8s.io/community/contributors/devel/sig-architecture/api-conventions.md#types-kinds
+        /// </summary>
+        [JsonProperty(PropertyName = "kind")]
+        public string Kind { get; set; }
+
+        /// <summary>
+        /// Gets or sets standard object's metadata. More info:
+        /// https://git.k8s.io/community/contributors/devel/sig-architecture/api-conventions.md#metadata
+        /// </summary>
+        [JsonProperty(PropertyName = "metadata")]
+        public V1ObjectMeta Metadata { get; set; }
+
+        /// <summary>
+        /// Gets or sets specification of the desired behavior of the service. More
+        /// info:
+        /// https://git.k8s.io/community/contributors/devel/sig-architecture/api-conventions.md#spec-and-status
+        /// </summary>
+        [JsonProperty(PropertyName = "spec")]
+        public ServiceSpec Spec { get; set; }
+
+        /// <summary>
+        /// Gets or sets most recently observed status of the service. This data
+        /// may not be up to date. Populated by the system. Read-only. More
+        /// info:
+        /// https://git.k8s.io/community/contributors/devel/sig-architecture/api-conventions.md#spec-and-status
+        /// </summary>
+        [JsonProperty(PropertyName = "status")]
+        public ServiceStatus Status { get; set; }
+
+        /// <summary>
+        /// Validate the object.
+        /// </summary>
+        /// <exception cref="Microsoft.Rest.ValidationException">
+        /// Thrown if validation fails
+        /// </exception>
+        public virtual void Validate()
+        {
+            if (Spec != null)
+            {
+                Spec.Validate();
+            }
+        }
+    }
+
+}
