707d5fb687e14ec990253c8c5609336f39a5960b
Mahdi <mahdi3466@yahoo.com>
2021-04-24 22:31:30 +0430

    WIP Knative Objects.

--- Kmanual/Services/KubernetesService.cs
+++ Kmanual/Services/KubernetesService.cs
@@ -1,7 +1,10 @@
 using Grpc.Core;
+using k8s;
+using k8s.Models;
 using Kmanual;
 using Kmanual.Docker;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,19 +15,45 @@ namespace Kmanual.Services
     public class KubernetesService : Kubernetes.KubernetesBase
     {
         private readonly ILogger<KubernetesService> _logger;
+        private readonly k8s.Kubernetes client;
+
 
         public KubernetesService(ILogger<KubernetesService> logger)
         {
             _logger = logger;
+
+            //var config = KubernetesClientConfiguration.InClusterConfig();
+            var config = KubernetesClientConfiguration.BuildConfigFromConfigFile(@"C:\Users\Mahdi\Desktop\config");
+            client = new k8s.Kubernetes(config);
         }
 
-        public override Task<GetNamespaceListResponse> GetNamespaceList(GetNamespaceListReuqest request, ServerCallContext context)
+        public override async Task<GetNamespaceListResponse> GetNamespaceList(GetNamespaceListReuqest request, ServerCallContext context)
         {
             var response = new GetNamespaceListResponse();
 
-            response.Namespaces.Add("default");
+            var namespaces = await client.ListNamespaceAsync();
 
-            return Task.FromResult(response);
+            response.Namespaces.AddRange(from ns in namespaces.Items select ns.Metadata.Name);
+
+            return response;
+        }
+
+        public override async Task<GetKServiceListResponse> GetKServiceList(GetKServiceListReuqest request, ServerCallContext context)
+        {
+            var response = new GetKServiceListResponse();
+
+            if (await client.ListNamespacedCustomObjectAsync("serving.knative.dev", "v1", "default", "services") is JObject services)
+            {
+                if (services.GetValue("items") is JArray items)
+                {
+                    foreach (var item in items)
+                    {
+                        response.Services.Add(item.ToString());
+                    }
+                }
+            }
+
+            return response;
         }
     }
 }
