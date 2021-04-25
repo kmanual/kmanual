cc565d93a18cdd1502958770ace855ed5b111d67
Mahdi <mahdi3466@yaho.com>
2021-04-25 21:43:35 +0430

    Service status.

--- Knative/ServiceStatus.cs
+++ Knative/ServiceStatus.cs
@@ -8,5 +8,32 @@ namespace Knative
 {
     public class ServiceStatus
     {
+        public StatusAddress Address { get; set; }
+        public Condition[] Conditions { get; set; }
+        public string LatestCreatedRevisionName { get; set; }
+        public string LatestReadyRevisionName { get; set; }
+        public int ObservedGeneration { get; set; }
+        public StatusTraffic[] Traffic { get; set; }
+        public string Url { get; set; }
+
+        public class StatusAddress
+        {
+            public string Url { get; set; }
+        }
+
+        public class Condition
+        {
+            public DateTime LastTransitionTime { get; set; }
+            public string Status { get; set; }
+            public string Type { get; set; }
+        }
+
+        public class StatusTraffic
+        {
+            public bool LatestRevision { get; set; }
+            public int Percent { get; set; }
+            public string RevisionName { get; set; }
+        }
+
     }
 }
